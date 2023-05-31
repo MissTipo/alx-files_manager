8e2897c47fcfc26b00fc452def6224709f4f5638
MissTipo <dorine.a.tipo@gmail.com>
2023-05-31 05:51:20 +0300

    eslint fix

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectId } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
@@ -8,63 +8,66 @@ const DB = `${DB_DATABASE}`;
 const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB}`;
 
 class DBClient {
-    constructor() {
-        this.client = new MongoClient(url, { useUnifiedTopology: true });
-        this.connected = false;
-        this.DB = null;
-        this.connect();
-    }
+  constructor () {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.connected = false;
+    this.DB = null;
+    this.connect();
+  }
 
-    async connect() {
-        try {
-            await this.client.connect();
-            this.DB = this.client.db(DB);
-            this.connected = true;
-            console.log('Connected to MongoDB');
-        } catch (error) {
-            console.error('Error connecting to MongoDB:', error);
-            throw error;
-        }
+  async connect () {
+    try {
+      await this.client.connect();
+      this.DB = this.client.db(DB);
+      this.connected = true;
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      throw error;
     }
+  }
 
-    isAlive() {
-        return this.connected;
-    }
+  isAlive () {
+    return this.connected;
+  }
 
-    async nbUsers() {
-        try {
-            const count = await this.DB.collection('users').countDocuments({});
-            return count;
-        } catch (error) {
-            console.error('Error retrieving number of users:', error);
-            throw error;
-        }
+  async nbUsers () {
+    try {
+      const count = await this.DB.collection('users').countDocuments({});
+      return count;
+    } catch (error) {
+      console.error('Error retrieving number of users:', error);
+      throw error;
     }
+  }
 
-    async nbFiles() {
-        try {
-            const count = await this.DB.collection('files').countDocuments({});
-            return count;
-        } catch (error) {
-            console.error('Error retrieving number of files:', error);
-            throw error;
-        }
+  async nbFiles () {
+    try {
+      const count = await this.DB.collection('files').countDocuments({});
+      return count;
+    } catch (error) {
+      console.error('Error retrieving number of files:', error);
+      throw error;
     }
-    //getting user by email
-    async getuser(email){
-    const user = await this.client.db('files_manager').collection('users').findOne({email});
+  }
+
+  // getting user by email
+  async getuser (email) {
+    const user = await this.client.db('files_manager').collection('users').findOne({ email });
     console.log(user);
     return user;
-    }
-    //creeate user
-    async createUser (email, password) {
-      const user = await this.client.db('files_manager').collection('users').insertOne({ email: email, password: password }); 
-      return user.insertedId;
-    }
-    async getuserId(token){
-        const user = await  this.client.db('files_manager').collection('users').findOne({_id:ObjectId(token)});
-        return user;
-    }
+  }
+
+  // creeate user
+  async createUser (email, password) {
+    const user = await this.client.db('files_manager').collection('users').insertOne({ email: email, password: password });
+    return user.insertedId;
+  }
+
+  async getuserId (token) {
+    const user = await this.client.db('files_manager').collection('users').findOne({ _id: ObjectId(token) });
+    return user;
+  }
 }
 
 const dbClient = new DBClient();
