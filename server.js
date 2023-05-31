f1613dd3d0136033acebff783e3792c0730ce54a
erickadikah <erickadikah2030@gmail.com>
2023-06-01 01:13:49 +0300

    express server fixed  Error

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 //Express server
-
-import express, { json } from 'express';
 import routes from './routes';
+import express from 'express';
 
-
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
